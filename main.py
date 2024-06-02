b2d7a0cdcf73c635af3c76b12bad5b30feee7253
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-06-02 19:59:05 +0300

    update_7

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import logging
+
 from faker import Faker
 import random
 import psycopg2
+from psycopg2 import DatabaseError
 
 fake = Faker()
 
@@ -14,4 +17,20 @@ for teacher_id in range(1, 4):
     for _ in range(2):
         cur.execute("INSERT INTO subjects (name, teacher_id) VALUES (%s, %s)", (fake.word(), teacher_id))
 
+for group_id in range(1, 4):
+    for _ in range(10):
+        cur.execute("INSERT INTO students (full_name, group_id) VALUES (%s, %s) RETURNING id", (fake.name(), group_id))
+        student_id = cur.fetchone()[0]
+        for subject_id in range(1, 7):
+            for _ in range(3):
+                cur.execute("INSERT INTO grades (student_id, subject_id, grade, grade_date) VALUES (%s, %s, %s, %s)",
+                            (student_id, subject_id, random.randint(0, 100), fake.date_this_decade()))
 
+try:
+    conn.commit()
+except DatabaseError as err:
+    logging.error(err)
+    conn.rollback()
+finally:
+    cur.close()
+    conn.close()
