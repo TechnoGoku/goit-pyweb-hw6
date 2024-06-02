9f3633b2a44d7b3b92055c854fdee4588976a24a
Ilya <159926377+TechnoGoku@users.noreply.github.com>
2024-06-02 22:39:03 +0300

    update_8

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ cur = conn.cursor()
 for _ in range(3):
     cur.execute("INSERT INTO groups (name) VALUES (%s)", (fake.word(),))
 
+for _ in range(3):
+    cur.execute("INSERT INTO teachers (fullname) VALUES (%s)", (fake.name(),))
+
 for teacher_id in range(1, 4):
     for _ in range(2):
         cur.execute("INSERT INTO subjects (name, teacher_id) VALUES (%s, %s)", (fake.word(), teacher_id))
